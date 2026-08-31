b5ce77079eb2c6282411f194fff6d5d4ea9ded13
ALAN-SIMTH-LIV <146905929+ALAN-SIMTH-LIV@users.noreply.github.com>
2026-08-31 17:43:43 +0800

    初始化

---
+++ src/test/java/com/demo/javaFfmpeg/JavaFfmpegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.javaFfmpeg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFfmpegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
